9dfd9ef7b032ac2245a4bd5f2c81d2cc1f7402f2
Peter Kang <peter@spell.run>
2020-04-19 01:59:04 -0400

    Retrieve board size from API

--- go/server/responses/game.go
+++ go/server/responses/game.go
@@ -3,7 +3,7 @@ package responses
 import "net/http"
 
 type Game struct {
-	Size int
+	Size int `json:"size"`
 }
 
 func (resp *Game) Render(_ http.ResponseWriter, _ *http.Request) error {
--- js/src/containers/Game/index.tsx
+++ js/src/containers/Game/index.tsx
@@ -1,17 +1,62 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Board from 'components/Board';
+import Board from "components/Board";
 
 interface Props {
   className: string;
 }
 
-class Game extends React.Component<Props> {
+interface State {
+  loading: boolean;
+  error: string;
+  gameInfo: GameInfo | null;
+}
+
+interface GameInfo {
+  size: number;
+}
+
+class Game extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      error: "",
+      gameInfo: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${process.env.API_URL}/catan/game`)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          loading: false,
+          gameInfo: data,
+        });
+      })
+      .catch(err => {
+        this.setState({
+          loading: false,
+          error: err,
+        });
+      });
+  }
+
   render() {
+    const { gameInfo } = this.state;
     return (
-      <div className={this.props.className}>
-        <Board size={3} />
-      </div>
+      !!gameInfo && (
+        <div className={this.props.className}>
+          <Board size={gameInfo.size} />
+        </div>
+      )
     );
   }
 }
