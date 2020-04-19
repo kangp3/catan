7dc63f43eeededf497bce67933c233e9f107bddb
Peter Kang <peter@spell.run>
2020-04-19 01:59:32 -0400

    Add script to automatically hot-load Go changes

---
+++ dev/gowatch.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+HERE="$(dirname "${BASH_SOURCE[0]}")"
+
+godir="$HERE/../go"
+while true; do
+    file=$(fswatch -1 "$godir" -e ".*" -i "\\.go$";)
+    echo 'Detected go file changes'
+
+    # Clean up existing processes
+    running_wait_pids="$(jobs -rp)"
+    if [[ $running_wait_pids ]]; then
+        kill $running_wait_pids;
+    fi
+
+    # Catch build errors locally and print, otherwise restart server
+    errors=$(
+        cd "$godir" && \
+        go install catan 2>&1 || true
+    )
+    if [[ -z $errors ]]; then
+        $HOME/go/bin/catan &
+    else
+        echo "$errors"
+    fi
+done
